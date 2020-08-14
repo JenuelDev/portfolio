0848c34f67974f1aab1f5f01a8437e31ab6c5140
Jenuel Oras Ganawed <jenuelganawed936@gmail.com>
2020-08-15 00:38:16 +0800

    updated porfolio
    fixed something

--- calculator-vue/src/components/footer/footer.vue
+++ calculator-vue/src/components/footer/footer.vue
@@ -1,6 +1,6 @@
 <template>
      <div class="footer">
-          Hi! I am <i>Jenuel Oras Ganawed</i> , I made this using vanilla JS's own library called speechSytnthesis. You can check about me 👉👉<a href="http://jenuelganawed.ml">CLICK ME</a>
+          Hi! I am <i>Jenuel Oras Ganawed</i> , I made this using VueJS with SASS. You can check about me 👉👉<a href="http://jenuelganawed.ml">CLICK ME</a>
      </div>
 </template>
 <script>
